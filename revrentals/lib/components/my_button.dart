02c4a243c48cd45df3e7929bb05d244885e4ad82
ktfr <112982272+ktfr@users.noreply.github.com>
2024-10-28 22:25:09 -0600

    Update Login page

---
+++ revrentals/lib/components/my_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          borderRadius: BorderRadius.circular(10),
+          ),
+        child: Center(child: Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            ),
+          )),
+      ),
+    );
+  }
+}
