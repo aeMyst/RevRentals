2e8ee2607c1811d36f9cfd8e608c5e33d0343be8
ktfr <kaiferrer8@gmail.com>
2024-12-03 18:32:43 -0700

    Update lot page

--- revrentals/lib/user/item_details/lot/lot.dart
+++ revrentals/lib/user/item_details/lot/lot.dart
@@ -1,6 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:revrentals/user/item_details/lot/lot_details.dart';
 // Widget to display motorcycles in each tab
+
+
+class LotTab extends StatelessWidget {
+  final Future<List<dynamic>> storageLotsFuture;
+  final int profileId;
+
+  const LotTab(
+      {super.key, required this.profileId, required this.storageLotsFuture});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<dynamic>>(
+      future: storageLotsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          final storageLots = snapshot.data!;
+          return ListView.builder(
+            itemCount: storageLots.length,
+            itemBuilder: (context, index) {
+              final lot = storageLots[index];
+              return ListTile(
+                title: Text("Lot No: ${lot['Lot_No']}"),
+                subtitle: Text("Address: ${lot['LAddress']}"),
+                trailing: const Icon(Icons.warehouse),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LotDetailsPage(profileId: profileId,
+                        lotData: lot,
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        } else {
+          return const Center(child: Text("No storage lots found."));
+        }
+      },
+    );
+  }
+}
+/*
 class LotTab extends StatelessWidget {
   const LotTab({super.key});
 
@@ -298,4 +347,4 @@ class _RentedLotCardState extends State<RentedLotCard> {
   Widget build(BuildContext context) {
     return const Placeholder();
   }
-}
+} */
